9a066d593b662812844fab0f87a7c9cf74832529
Daisuke Nakahara <daisuken@msn.com>
2020-09-09 23:51:36 -0700

    Test Script

--- tsi-setup.sh
+++ tsi-setup.sh
@@ -1,4 +1,6 @@
-spName='TSI-SP-02'
+echo off
+
+spName='TSI-SP-03'
 servicePrincipalAppId=$(az ad app create --display-name $spName --identifier-uris "https://$spName"  --oauth2-allow-implicit-flow true --required-resource-accesses '[{"resourceAppId":"120d688d-1518-4cf7-bd38-182f158850b6","resourceAccess":[{"id":"a3a77dfe-67a4-4373-b02a-dfe8485e2248","type":"Scope"}]}]' --query appId -o tsv)
 servicePrincipalObjectId=$(az ad sp create --id $servicePrincipalAppId --query objectId -o tsv)
 servicePrincipalSecret=$(az ad app credential reset --id $servicePrincipalAppId --credential-description "TSI Token" --query password -o tsv)
