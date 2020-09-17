91373f37da9a78edc4c1719fa33e1e173461041d
Daisuke Nakahara <daisuken@msn.com>
2020-09-16 17:30:31 -0700

    Update tsi-setup.sh

--- tsi-setup.sh
+++ tsi-setup.sh
@@ -12,11 +12,12 @@ if [ -z "$servicePrincipalObjectId" ]; then
     servicePrincipalObjectId=$(az ad sp create --id $servicePrincipalAppId --query objectId -o tsv)
 fi
 
+echo 'Service Principal App Id    :' $servicePrincipalAppId
+echo 'Service Principal Password  :' $servicePrincipalSecret
+
 servicePrincipalSecret=$(az ad app credential reset --id $servicePrincipalAppId --credential-description "OpenPlatform TSI Token" --query password -o tsv)
 servicePrincipalTenantId=$(az ad sp show --id $servicePrincipalAppId --query appOwnerTenantId -o tsv)
 
-echo 'Service Principal App Id    :' $servicePrincipalAppId
-echo 'Service Principal Password  :' $servicePrincipalSecret
 echo 'Service Principal Tenant Id :' $servicePrincipalTenantId
 echo 'Service Principal Object Id :' $servicePrincipalObjectId
 
